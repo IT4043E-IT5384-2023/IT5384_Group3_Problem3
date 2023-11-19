c058be2df28f26dcbc3671a211734c38d26ebce0
VuQuynh27 <vu.diemquynh23@gmail.com>
2023-11-20 00:34:16 +0700

    Update mô tả dữ liệu

--- twitter_scraper_selenium/main.py
+++ twitter_scraper_selenium/main.py
@@ -1,29 +1,25 @@
-# from twitter_scraper_selenium.profile_details import get_profiles_detailslist
-# import csv
-# import time
-#
-# def get_records_by_field(csv_file, field_name):
-#     records = []
-#
-#     with open(csv_file, 'r', newline='') as file:
-#         csv_reader = csv.DictReader(file)
-#
-#         for row in csv_reader:
-#             if field_name in row:
-#                 records.append(row[field_name])
-#
-#     return records
-#
-#
-# twitter_username = get_records_by_field('Username.csv','screenName')
-#
-#
-# output_file = "twitter_api_data"
-#
-#
-# get_profiles_detailslist(twitter_usernames=twitter_username, filename=output_file)
+from twitter_scraper_selenium.profile_details import get_profiles_detailslist
+import csv
+import time
+
+def get_records_by_field(csv_file, field_name):
+    records = []
+
+    with open(csv_file, 'r', newline='') as file:
+        csv_reader = csv.DictReader(file)
+
+        for row in csv_reader:
+            if field_name in row:
+                records.append(row[field_name])
+
+    return records
 
 
-from twitter_scraper_selenium import scrape_profile_with_api
+twitter_username = get_records_by_field('Username.csv','screenName')
 
-scrape_profile_with_api('elonmusk', output_filename='musk', tweets_count= 10)
+
+output_file = "twitter_api_data"
+
+
+get_profiles_detailslist(twitter_usernames=twitter_username, filename=output_file)
+
