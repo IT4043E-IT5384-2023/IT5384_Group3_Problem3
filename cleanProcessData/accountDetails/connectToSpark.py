a728eff6a08d3782038b675219f2760c3af61c39
VuQuynh27 <vu.diemquynh23@gmail.com>
2023-12-29 10:19:14 +0700

    update code

--- cleanProcessData/accountDetails/connectToSpark.py
+++ cleanProcessData/accountDetails/connectToSpark.py
@@ -1,16 +1,49 @@
-from pyspark.sql import SparkSession
+# from pyspark.sql import SparkSession
 from pyspark.sql.functions import col
+#
+# # Khởi tạo Spark Session
+# # spark = (SparkSession.builder.appName("ConvertJSONtoCSV").master("local[*]")
+# spark = (SparkSession.builder.appName("SimpleSparkJob").master("http://34.142.194.212:8000/")
+#          .config("spark.executor.memory", "2G")  #excutor excute only 2G
+#         .config("spark.driver.memory","4G")
+#         .config("spark.executor.cores","1") #Cluster use only 3 cores to excute
+#         .config("spark.python.worker.memory","1G") # each worker use 1G to excute
+#         .config("spark.driver.maxResultSize","3G") #Maximum size of result is 3G
+#         .config("spark.kryoserializer.buffer.max","1024M")
+#          .getOrCreate()) #http://34.142.194.212:4040
 
-# Khởi tạo Spark Session
-# spark = (SparkSession.builder.appName("ConvertJSONtoCSV").master("local[*]")
+
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType
+#config the connector jar file
 spark = (SparkSession.builder.appName("SimpleSparkJob").master("spark://34.142.194.212:7077")
+         .config("spark.jars", "/opt/spark/jars/gcs-connector-latest-hadoop2.jar")
          .config("spark.executor.memory", "2G")  #excutor excute only 2G
         .config("spark.driver.memory","4G")
-        .config("spark.executor.cores","1") #Cluster use only 3 cores to excute
+        .config("spark.executor.cores","1") #Cluster use only 3 cores to excute as it has 3 server
         .config("spark.python.worker.memory","1G") # each worker use 1G to excute
-        .config("spark.driver.maxResultSize","3G") #Maximum size of result is 3G
+        .config("spark.driver.maxResultSize","2G") #Maximum size of result is 3G
         .config("spark.kryoserializer.buffer.max","1024M")
-         .getOrCreate()) #http://34.142.194.212:4040
+        .config("spark.hadoop.fs.gs.impl", "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystem")
+        .config("spark.hadoop.google.cloud.auth.service.account.enable", "true")
+        # .config("spark.hadoop.google.cloud.auth.service.account.json.keyfile", "/opt/bucket_connector/lucky-wall-393304-2a6a3df38253.json")
+        .config('spark.debug.maxToStringFields', 100)
+        .config("spark.jars.packages", "com.google.cloud.bigdataoss:gcs-connector:hadoop3-2.2.0")
+
+         .getOrCreate())
+#config the credential to identify the google cloud hadoop file
+# spark.conf.set("google.cloud.auth.service.account.json.keyfile","/opt/bucket_connector/lucky-wall-393304-2a6a3df38253.json")
+# spark._jsc.hadoopConfiguration().set('fs.gs.impl', 'com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystem')
+# spark._jsc.hadoopConfiguration().set('fs.gs.auth.service.account.enable', 'true')
+
+## Connect to the file in Google Bucket with Spark
+
+#path= f"gs://it4043e-it5384/it5384/IT5384_Group2_Problem1/Data1.json"
+#df =spark.read.json(path, multiLine=True)
+#df.show()
+
+#spark.stop() # Ending spark job
+
 # Đọc dữ liệu JSON thành DataFrame
 # df = spark.read.json('C:/Users/DELL/PycharmProjects/IT5384_Group3_Problem3/twitter_scraper_selenium/Accounts_details/realAcc.json')
 df = spark.read.json('C:/Users/DELL/PycharmProjects/IT5384_Group3_Problem3/twitter_scraper_selenium/Accounts_details/realAcc.json', multiLine=True)
