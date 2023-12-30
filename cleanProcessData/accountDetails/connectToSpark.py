a48b428eb880518913007c5445350bc56dec30fe
VuQuynh27 <vu.diemquynh23@gmail.com>
2023-12-30 16:04:45 +0700

    Update score code

--- cleanProcessData/accountDetails/connectToSpark.py
+++ cleanProcessData/accountDetails/connectToSpark.py
@@ -1,40 +1,65 @@
-# from pyspark.sql import SparkSession
-from pyspark.sql.functions import col
-#
-# # Khởi tạo Spark Session
-# # spark = (SparkSession.builder.appName("ConvertJSONtoCSV").master("local[*]")
-# spark = (SparkSession.builder.appName("SimpleSparkJob").master("http://34.142.194.212:8000/")
-#          .config("spark.executor.memory", "2G")  #excutor excute only 2G
-#         .config("spark.driver.memory","4G")
-#         .config("spark.executor.cores","1") #Cluster use only 3 cores to excute
-#         .config("spark.python.worker.memory","1G") # each worker use 1G to excute
-#         .config("spark.driver.maxResultSize","3G") #Maximum size of result is 3G
-#         .config("spark.kryoserializer.buffer.max","1024M")
-#          .getOrCreate()) #http://34.142.194.212:4040
-
-
+import pyspark.sql as spark
+from pyspark.sql.functions import concat_ws
 from pyspark.sql import SparkSession
+
+# Create Spark Session
+spark = spark.SparkSession.builder.appName("ETLJob").getOrCreate()
+# spark = (SparkSession.builder.appName("SimpleSparkJob").master("spark://34.142.194.212:7077")
+#          .config("spark.jars", "/opt/spark/jars/gcs-connector-latest-hadoop2.jar")
+#          .config("spark.executor.memory", "2G")  #excutor excute only 2G
+#          .config("spark.driver.memory","4G")
+#          .config("spark.executor.cores","1") #Cluster use only 3 cores to excute as it has 3 server
+#          .config("spark.python.worker.memory","1G") # each worker use 1G to excute
+#          .config("spark.driver.maxResultSize","2G") #Maximum size of result is 3G
+#          .config("spark.kryoserializer.buffer.max","1024M")
+#          .config("spark.hadoop.fs.gs.impl", "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystem")
+#          .config("spark.hadoop.google.cloud.auth.service.account.enable", "true")
+#         # .config("spark.hadoop.google.cloud.auth.service.account.json.keyfile", "/opt/bucket_connector/lucky-wall-393304-2a6a3df38253.json")
+#          .config('spark.debug.maxToStringFields', 100)
+#          .config("spark.jars.packages", "com.google.cloud.bigdataoss:gcs-connector:hadoop3-2.2.0")
+#
+#          .getOrCreate())
+spark.conf.set("google.cloud.auth.service.account.json.keyfile","/opt/bucket_connector/lucky-wall-393304-2a6a3df38253.json")
+spark._jsc.hadoopConfiguration().set('fs.gs.impl', 'com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystem')
+spark._jsc.hadoopConfiguration().set('fs.gs.auth.service.account.enable', 'true')
+
+# Read data from parquet file
+df = spark.read.parquet("C:/Users/DELL/Downloads/employees/part-00000-9abf32a3-db43-42e1-9639-363ef11c0d1c-c000.snappy.parquet")
+df.show(10)
+
+# Transform data
+# df = df.withColumn("fullname", spark.sql("concat(first_name, ' ', last_name)"))
+df = df.withColumn("fullname", concat_ws(" ", df.first_name, df.second_name))
+df = df.withColumn("seat_height", df.floor * 2.5)
+
+# Save data to CSV file
+df.write.mode("overwrite").csv("employees.csv")
+
+df.show(10)
+
+# Stop Spark Session
+spark.stop()
+
+
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType
 #config the connector jar file
-spark = (SparkSession.builder.appName("SimpleSparkJob").master("spark://34.142.194.212:7077")
-         .config("spark.jars", "/opt/spark/jars/gcs-connector-latest-hadoop2.jar")
-         .config("spark.executor.memory", "2G")  #excutor excute only 2G
-        .config("spark.driver.memory","4G")
-        .config("spark.executor.cores","1") #Cluster use only 3 cores to excute as it has 3 server
-        .config("spark.python.worker.memory","1G") # each worker use 1G to excute
-        .config("spark.driver.maxResultSize","2G") #Maximum size of result is 3G
-        .config("spark.kryoserializer.buffer.max","1024M")
-        .config("spark.hadoop.fs.gs.impl", "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystem")
-        .config("spark.hadoop.google.cloud.auth.service.account.enable", "true")
-        # .config("spark.hadoop.google.cloud.auth.service.account.json.keyfile", "/opt/bucket_connector/lucky-wall-393304-2a6a3df38253.json")
-        .config('spark.debug.maxToStringFields', 100)
-        .config("spark.jars.packages", "com.google.cloud.bigdataoss:gcs-connector:hadoop3-2.2.0")
-
-         .getOrCreate())
+# spark = (SparkSession.builder.appName("SimpleSparkJob").master("spark://34.142.194.212:7077")
+        #  .config("spark.jars", "/opt/spark/jars/gcs-connector-latest-hadoop2.jar")
+        #  .config("spark.executor.memory", "2G")  #excutor excute only 2G
+        # .config("spark.driver.memory","4G")
+        # .config("spark.executor.cores","1") #Cluster use only 3 cores to excute as it has 3 server
+        # .config("spark.python.worker.memory","1G") # each worker use 1G to excute
+        # .config("spark.driver.maxResultSize","2G") #Maximum size of result is 3G
+        # .config("spark.kryoserializer.buffer.max","1024M")
+        # .config("spark.hadoop.fs.gs.impl", "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystem")
+        # .config("spark.hadoop.google.cloud.auth.service.account.enable", "true")
+        # # .config("spark.hadoop.google.cloud.auth.service.account.json.keyfile", "/opt/bucket_connector/lucky-wall-393304-2a6a3df38253.json")
+        # .config('spark.debug.maxToStringFields', 100)
+        # .config("spark.jars.packages", "com.google.cloud.bigdataoss:gcs-connector:hadoop3-2.2.0")
+        #
+        #  .getOrCreate())
 #config the credential to identify the google cloud hadoop file
-# spark.conf.set("google.cloud.auth.service.account.json.keyfile","/opt/bucket_connector/lucky-wall-393304-2a6a3df38253.json")
-# spark._jsc.hadoopConfiguration().set('fs.gs.impl', 'com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystem')
-# spark._jsc.hadoopConfiguration().set('fs.gs.auth.service.account.enable', 'true')
+
 
 ## Connect to the file in Google Bucket with Spark
 
@@ -43,41 +68,3 @@ spark = (SparkSession.builder.appName("SimpleSparkJob").master("spark://34.142.1
 #df.show()
 
 #spark.stop() # Ending spark job
-
-# Đọc dữ liệu JSON thành DataFrame
-# df = spark.read.json('C:/Users/DELL/PycharmProjects/IT5384_Group3_Problem3/twitter_scraper_selenium/Accounts_details/realAcc.json')
-df = spark.read.json('C:/Users/DELL/PycharmProjects/IT5384_Group3_Problem3/twitter_scraper_selenium/Accounts_details/realAcc.json', multiLine=True)
-
-
-# Chọn các cột cần thiết
-df_selected = df.select(
-    col("user.result.rest_id").alias("rest_id"),
-    col("user.result.legacy.screen_name").alias("screen_name"),
-    col("user.result.is_blue_verified").alias("is_blue_verified"),
-    col("user.result.legacy.description").alias("description"),
-    col("user.result.legacy.location").alias("location"),
-    col("user.result.legacy.entities.description.urls.url").alias("url"),
-    col("user.result.legacy.created_at").alias("join_time"),
-    col("user.result.legacy.default_profile_image").alias("default_profile_image"),
-    col("user.result.legacy.statuses_count").alias("statuses_count"),
-    col("user.result.legacy.friends_count").alias("friends_count"),
-    col("user.result.legacy.followers_count").alias("followers_count"),
-    col("user.result.legacy.favourites_count").alias("favourites_count"),
-    col("user.result.legacy.listed_count").alias("listed_count"),
-    col("user.result.legacy.media_count").alias("media_count"),
-    col("user.result.legacy.possibly_sensitive").alias("possibly_sensitive"),
-    col("user.result.legacy.protected").alias("private"),
-    col("user.result.legacy.verified").alias("verified"),
-    col("user.result.legacy.profile_image_url_https").alias("profile_image_url_https"),
-    col("user.result.legacy.profile_banner_url").alias("profile_banner_url"),
-    col("").alias("label")  # Thêm cột label với giá trị rỗng
-)
-
-# Ghi DataFrame ra CSV
-csv_file_path = 'cleanProfileData1.csv'
-df_selected.write.csv(csv_file_path, header=True, mode="overwrite")
-
-print(f'Data has been written to {csv_file_path}')
-
-# Dừng SparkSession
-spark.stop()
--- scoreProcess/profileScore/scoreFile.py
+++ scoreProcess/profileScore/scoreFile.py
@@ -1,180 +1,83 @@
-import csv
+import pandas as pd
 import datetime
 import re
-# Tạo danh sách các trường với kiểu dữ liệu int
-fields = [
-    "rest_id"
-    "followerCount",
-    "friend_followerRatio",
-    "tweetFrequency",
-    "tweet_followerRatio",
-    "numOfFriends",
-    "dateOfLastTweet",
-    "verifiedAccount",
-    "numOfLists",
-    "favoritedCount",
-    "accountAge",
-    "bioCompleteness",
-    "profilePicture",
-    "protectedAccount",
-    "totalScore"
-]
 
-# Mở file CSV để ghi
-with open("score_data.csv", "w", newline="") as csvfile:
-   writer = csv.writer(csvfile)
-
-   # Ghi hàng tiêu đề
-   writer.writerow(fields)
-
-   # Thêm dữ liệu mẫu (bạn có thể thay thế bằng dữ liệu thực tế)
-   writer.writerow([
-       '',
-       -1,  # followerCount
-       -1,  # friend_followerRatio
-       -1,  # tweetFrequency
-       -1,  # tweet_followerRatio
-       -1,  # numOfFriends
-       -1,  # dateOfLastTweet
-       -1,  # verifiedAccount
-       -1,  # numOfLists
-       -1,  # favoritedCount
-       -1,  # accountAge
-       -1,  # bioCompleteness
-       -1,  # profilePicture
-       -1,  # protectedAccount
-       -1,
-    ])
-
-
-#
-# def calculate_scores(data_file, score_file):
-#    """
-#    Đọc các trường từ file data và tính toán cho các trường của file score
-#    """
-#
-#    with open(data_file, "r") as csvfile:
-#        reader = csv.DictReader(csvfile)
-#        scores = []
-#        for row in reader:
-#            score = {
-#                "rest_id": row["rest_id"],
-#                "followerCount": 10 if int(row["followers_count"]) >= 3000 else 5 if int(row["followers_count"]) >= 400 else 2 if int(row["followers_count"]) >= 50 else 0,
-#                "friend_followerRatio": 15 if float(row["friends_count"]) / float(row["followers_count"]) > 5 else 10 if float(row["friends_count"]) / float(row["followers_count"]) > 2 else 5 if float(row["friends_count"]) / float(row["followers_count"]) > 1 else 0,
-#                "tweetFrequency": -1,  # Chưa có logic tính tweetFrequency
-#                "tweet_followerRatio": 10 if float(row["statuses_count"]) / float(row["followers_count"]) < 100 else 0,
-#                "numOfFriends": 10 if int(row["friends_count"]) > 100 else 0,
-#                "dateOfLastTweet": -1,  # Chưa có dữ liệu dateOfLastTweet
-#                "verifiedAccount": 5 if row["verified"] == "true" else 0,
-#                "numOfLists": 5 if int(row["listed_count"]) > 5 else 0,
-#                "favoritesCount": 11 if int(row["favourites_count"]) > 2000 else 5,
-#                "accountAge": 5 if (datetime.date.today() - datetime.datetime.strptime(row["join_time"], "%Y-%m-%d").date()).days > 365 else 0,
-#                "bioCompleteness": 4 if row["description"] and row["location"] else 0,
-#                "profilePicture": 3 if row["default_profile_image"] != "null" else 0,
-#                "protectedAccount": 4 if row["private"] == "false" else 0,
-#                "totalScore": sum([value for key, value in score.items() if key != "rest_id"])
-#            }
-#            scores.append(score)
-#
-#    with open(score_file, "w", newline="") as csvfile:
-#        fieldnames = ["rest_id", "followerCount", "friend_followerRatio", "tweetFrequency", "tweet_followerRatio", "numOfFriends", "dateOfLastTweet", "verifiedAccount", "numOfLists", "favoritesCount", "accountAge", "bioCompleteness", "profilePicture", "protectedAccount"]
-#        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-#        writer.writeheader()
-#        writer.writerows(scores)
-
-
+def seconds_from_timedelta(time_delta):
+    return time_delta.total_seconds() if not pd.isnull(time_delta) else 0
 def calculate_scores(data_file, tweet_file, score_file):
-    """
-    Đọc các trường từ file data và tweet, tính toán cho các trường của file score
-    """
+    # Đọc dữ liệu từ tệp CSV bằng pandas
+    data_df = pd.read_csv(data_file)
+    tweet_df = pd.read_csv(tweet_file)
+    tweet_df['time_diff'] = pd.to_timedelta(tweet_df['time_diff'])
+    tweet_df['total_seconds'] = tweet_df['time_diff'].apply(seconds_from_timedelta)
+    # Tạo danh sách để lưu trữ điểm số và thêm tiêu đề
+    scores_list = [
+        [
+            "rest_id", "followerCount", "friend_followerRatio", "tweetFrequency",
+            "tweet_followerRatio", "numOfFriends", "dateOfLastTweet",
+            "verifiedAccount", "numOfLists", "favoritedCount", "accountAge",
+            "bioCompleteness", "profilePicture", "protectedAccount", "totalScore"
+        ]
+    ]
 
-    with open(data_file, "r", encoding="utf-8") as csvfile:
-        reader = csv.DictReader(csvfile)
-        scores = []
-        for row in reader:
-            score = {
-                "rest_id": row["rest_id"],
-                "followerCount": 10 if int(row["followers_count"]) >= 3000 else 5 if int(row["followers_count"]) >= 400 else 2 if int(row["followers_count"]) >= 50 else 0,
-                "friend_followerRatio": 0 if int(row["followers_count"]) == 0 else 15 if float(row["friends_count"]) / float(row["followers_count"]) > 5 else 10 if float(row["friends_count"]) / float(row["followers_count"]) > 2 else 5 if float(row["friends_count"]) / float(row["followers_count"]) > 1 else 0,
-                "tweetFrequency": 0,  # Chưa có logic tính tweetFrequency
-                "tweet_followerRatio": 0 if int(row["followers_count"]) == 0 else 10 if (float(row["statuses_count"]) / float(row["followers_count"])) < 50 else 5 if (float(row["statuses_count"]) / float(row["followers_count"])) < 100 else 0,
-                "numOfFriends": 10 if int(row["friends_count"]) > 100 else 5 if int(row["friends_count"]) > 50 else 0,
-                "dateOfLastTweet": 0,  # Chưa có dữ liệu dateOfLastTweet
-                "verifiedAccount": 5 if row["verified"] == "true" else 0,
-                "numOfLists": 5 if int(row["listed_count"]) > 5 else 3 if int(row["listed_count"]) > 2 else 0,
-                "favoritesCount": 11 if int(row["favourites_count"]) > 2000 else 5 if int(row["favourites_count"]) > 1000 else 0,
-                "accountAge": 5 if (datetime.date(2023, 11, 15) - datetime.datetime.strptime(row["join_time"], "%a %b %d %H:%M:%S %z %Y").date()).days > 365 else 3 if (datetime.date(2023, 11, 15) - datetime.datetime.strptime(row["join_time"], "%a %b %d %H:%M:%S %z %Y").date()).days > 90 else 0,
-                "bioCompleteness": 4 if row["description"] and row["location"] else 0,
-                "profilePicture": 3 if row["default_profile_image"] != "null" else 0,
-                "protectedAccount": 4 if row["private"] == "false" else 0,
-                # "totalScore": sum([value for key, value in score.items() if key != "rest_id"])
-            }
+    # Tạo một dictionary để lưu trữ tweets theo user_id
+    tweets_by_user_id = tweet_df.groupby("user_id").apply(lambda group: group.to_dict(orient="records")).to_dict()
 
+    for _, row in data_df.iterrows():
+        score = {
+            "rest_id": row["rest_id"],
+            "followerCount": 10 if row["followers_count"] >= 3000 else 5 if row["followers_count"] >= 400 else 2 if row["followers_count"] >= 50 else 0,
+             "friend_followerRatio": 0 if row["followers_count"] == 0 else 15 if row["friends_count"] / row["followers_count"] > 5 else 10 if row["friends_count"] / row["followers_count"] > 2 else 5 if row["friends_count"] / row["followers_count"] > 1 else 0,
+            "tweetFrequency": 0,  # Chưa có logic tính tweetFrequency
+            "tweet_followerRatio": 0 if row["followers_count"] == 0 else 10 if (row["statuses_count"] / row["followers_count"]) < 50 else 5 if (row["statuses_count"] / row["followers_count"]) < 100 else 0,
+            "numOfFriends": 10 if row["friends_count"] > 100 else 5 if row["friends_count"] > 50 else 0,
+            "dateOfLastTweet": 0,  # Chưa có dữ liệu dateOfLastTweet
+            "verifiedAccount": 5 if row["verified"] == "true" else 0,
+            "numOfLists": 5 if row["listed_count"] > 5 else 3 if row["listed_count"] > 2 else 0,
+            "favoritedCount": 11 if row["favourites_count"] > 2000 else 5 if row["favourites_count"] > 1000 else 0,
+            "accountAge": 5 if (datetime.date(2023, 11, 15) - pd.to_datetime(row["join_time"]).date()).days > 365 else 3 if (datetime.date(2023, 11, 15) - pd.to_datetime(row["join_time"]).date()).days > 90 else 0,
+            "bioCompleteness": 4 if pd.notna(row["description"]) and pd.notna(row["location"]) else 0,
+            "profilePicture": 3 if pd.notna(row["default_profile_image"]) and row["default_profile_image"] != "null" else 0,
+            "protectedAccount": 4 if row["private"] == "false" else 0,
+        }
 
-            # Tính tweetFrequency
-            tweets = []
-
-            with open(tweet_file, "r", encoding="utf-8") as csvfile:
-                reader = csv.DictReader(csvfile)
-                for tweet in reader:
-                    if tweet["user_id"] == row["rest_id"]:
-                        tweets.append(tweet)
-
-            if len(tweets) > 0:
-                total_seconds = []
-                time_diffs = [tweet["time_diff"] for tweet in tweets]
-                for time in time_diffs:
-                    time = str(time)
-                    if time == "":
-                        time = "0 days 00:00:00"
-                    days_match = re.search(r"(.*?) days", time)
-                    days = days_match.group(1)
-                    time_part = time[days_match.end():]
-
-                    # Split time part
-                    hours, minutes, seconds = time_part.split(":")
-                    seconds = int(days) * 86400 + int(hours) * 3600 + int(minutes) * 60 + int(seconds)
-                    total_seconds.append(seconds)
-                if len(time_diffs) > 0:
-                    average_time_diff = sum(total_seconds) / len(time_diffs)
-                    if 86400 / 6 > average_time_diff > 86400 / 12:
-                        score["tweetFrequency"] = 8
-                    elif average_time_diff > 86400 / 12:
-                        score["tweetFrequency"] = 4
-                    else:
-                        score["tweetFrequency"] = 0
+        # Tính tweetFrequency
+        tweets = tweets_by_user_id.get(row["rest_id"], [])
+        if len(tweets) > 0:
+            total_seconds = [tweet["total_seconds"] for tweet in tweets]
+            if len(total_seconds) > 0:
+                average_time_diff = sum(total_seconds) / len(total_seconds)
+                if 86400 / 6 > average_time_diff > 86400 / 12:
+                    score["tweetFrequency"] = 8
+                elif average_time_diff > 86400 / 12:
+                    score["tweetFrequency"] = 4
                 else:
                     score["tweetFrequency"] = 0
             else:
                 score["tweetFrequency"] = 0
+        else:
+            score["tweetFrequency"] = 0
 
-            # Tính dateOfLastTweet
-            if len(tweets) > 0:
-                # Tìm tweet có date gần đây nhất
-                date_of_last_tweet = min([datetime.datetime.strptime(tweet["date"], "%Y-%m-%d").date() for tweet in tweets])
+        # Tính dateOfLastTweet
+        if len(tweets) > 0:
+            date_of_last_tweet = min([pd.to_datetime(tweet["date"]).date() for tweet in tweets])
+            days_diff = (datetime.date(2023, 11, 15) - date_of_last_tweet).days
 
-                # Tính khoảng cách từ date gần nhất đến ngày 15/11/2023
-                days_diff = (datetime.date(2023, 11, 15) - date_of_last_tweet).days
+            if days_diff < 2:
+                score["dateOfLastTweet"] = 8
+            elif days_diff < 7:
+                score["dateOfLastTweet"] = 4
+            else:
+                score["dateOfLastTweet"] = 0
 
-                if days_diff < 2:
-                    score["dateOfLastTweet"] = 8
-                elif days_diff < 7:
-                    score["dateOfLastTweet"] = 4
-                else:
-                    score["dateOfLastTweet"] = 0
+        score["totalScore"] = sum(score.values()) - score["rest_id"]  # Trừ điểm của rest_id
+        scores_list.append(list(score.values()))
 
-            score["totalScore"] = sum([value for key, value in score.items() if key != "rest_id"])
-            scores.append(score)
+    # Tạo DataFrame từ danh sách điểm số và lưu vào tệp CSV
+    scores_df = pd.DataFrame(scores_list[1:], columns=scores_list[0])
+    scores_df.to_csv(score_file, index=False)
 
-    with open(score_file, "w", newline="") as csvfile:
-        fieldnames = ["rest_id", "followerCount", "friend_followerRatio", "tweetFrequency", "tweet_followerRatio", "numOfFriends", "dateOfLastTweet", "verifiedAccount", "numOfLists", "favoritesCount", "accountAge", "bioCompleteness", "profilePicture", "protectedAccount", "totalScore"]
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
-        writer.writerows(scores)
-
-
-# Thay thế tên file data và score thực tế
-data_file = "C:/Users/DELL/PycharmProjects/IT5384_Group3_Problem3/cleanProcessData/accountDetails/cleanProfileData.csv"
-tweet_file = "C:/Users/DELL/PycharmProjects/IT5384_Group3_Problem3/cleanProcessData/tweets/output_cleans.csv"
+data_file = "C:/Users/DELL/PycharmProjects/IT5384_Group3_Problem3/cleanProcessData/accountDetails/cleanProfileData2.csv"
+tweet_file = "C:/Users/DELL/PycharmProjects/IT5384_Group3_Problem3/cleanProcessData/tweets/output_cleans0.csv"
 score_file = "score_data.csv"
 calculate_scores(data_file, tweet_file, score_file)
